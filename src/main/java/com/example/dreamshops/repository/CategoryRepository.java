a8e91acc39683dd4ed317c0fe9999bb45ef33f45
Valeriia <45694534+valeriia-shurupina@users.noreply.github.com>
2024-10-02 21:43:01 +0200

    implement category service

---
+++ src/main/java/com/example/dreamshops/repository/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.example.dreamshops.repository;
+
+import com.example.dreamshops.model.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+    Category findByName(String name);
+    boolean existsByName(String name);
+}
---
+++ src/main/java/com/example/dreamshops/service/category/ICategoryService.java
@@ -0,0 +1,14 @@
+package com.example.dreamshops.service.category;
+
+import com.example.dreamshops.model.Category;
+
+import java.util.List;
+
+public interface ICategoryService {
+    Category getCategoryById(Long id);
+    Category getCategoryByName(String name);
+    List<Category> getAllCategories();
+    Category addCategory(Category category);
+    Category updateCategory(Category category, Long id);
+    void deleteCategoryById(Long id);
+}
