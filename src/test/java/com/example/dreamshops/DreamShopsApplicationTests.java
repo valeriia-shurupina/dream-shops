cb65df2cc42e10c5e85df698c4ddc609646f74ec
Valeriia <45694534+valeriia-shurupina@users.noreply.github.com>
2024-10-02 21:40:11 +0200

    Initial commit

---
+++ src/test/java/com/example/dreamshops/DreamShopsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dreamshops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DreamShopsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
